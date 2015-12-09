c58a02ccd0ce8bf5cd08f9380d2afa9c6bdd4d20
jonpas <jonpas33@gmail.com>
2015-12-09 19:07:07 +0100

    Fix ADDON macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,4 +1,11 @@
-// https://github.com/CBATeam/CBA_A3/blob/d7dc8b7e49db31aa52ff7eec4e2c27fbf310fe45/addons/main/script_macros_common.hpp#L62-L64
-#ifndef VERSION_CONFIG
-    #define VERSION_CONFIG version = VERSION; versionStr = QUOTE(VERSION); versionAr[] = {VERSION_AR}
-#endif
+/*
+    CBA Framework Extracts
+    https://github.com/CBATeam/CBA_A3/blob/master/addons/main/script_macros_common.hpp
+*/
+
+// General
+#define VERSION_CONFIG version = VERSION; versionStr = QUOTE(VERSION); versionAr[] = {VERSION_AR}
+#define ADDON DOUBLES(PREFIX,COMPONENT)
+
+// Internal Functions
+#define DOUBLES(var1,var2) ##var1##_##var2
