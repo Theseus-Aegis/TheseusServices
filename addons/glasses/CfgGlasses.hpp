8c6b15ddf35131bb1832e8c6799b2e6ab26c00cb
jonpas <jonpas33@gmail.com>
2015-12-13 22:15:23 +0100

    Improved and finalized Goggles

--- addons/glasses/CfgGlasses.hpp
+++ addons/glasses/CfgGlasses.hpp
@@ -1,34 +1,32 @@
 class CfgGlasses {
-    class G_Tactical_Clear;
-    class CLASS(Goggles_Base): G_Tactical_Clear {
-        dlc = DLC;
-        scope = 0;
-        identityTypes[] = {};
-        //hiddenSelections[] = {"camo"};
-    };
-
-    class CLASS(Goggles_Black): CLASS(Goggles_Base) {
+    class G_Combat;
+    class CLASS(Goggles_Black): G_Combat {
         _generalMacro = QCLASS(Goggles_Black);
+        dlc = DLC;
         scope = 2;
+        identityTypes[] = {};
         author = "Pomigit, Jonpas";
         displayname = CSTRING(Goggles_Black);
-        model = QPATHTOF(data\goggles_black.p3d);
-        //hiddenSelectionsTextures[] = {QPATHTOF(data\goggles_sand_ca.paa)};
+        model = QPATHTOF(data\goggles.p3d);
+        mass = 2;
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\goggles_black_ca.paa)};
+        // ACE3
+        ace_overlay = "";
+        ace_resistance = 2;
     };
-
-    class CLASS(Goggles_Green): CLASS(Goggles_Base) {
+    class CLASS(Goggles_Green): CLASS(Goggles_Black) {
         _generalMacro = QCLASS(Goggles_Green);
         scope = 2;
         author = "Pomigit, Jonpas";
         displayname = CSTRING(Goggles_Green);
-        model = QPATHTOF(data\goggles_green.p3d);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\goggles_green_ca.paa)};
     };
-
-    class CLASS(Goggles_Sand): CLASS(Goggles_Base) {
+    class CLASS(Goggles_Sand): CLASS(Goggles_Black) {
         _generalMacro = QCLASS(Goggles_Sand);
         scope = 2;
         author = "Pomigit, Jonpas";
         displayname = CSTRING(Goggles_Sand);
-        model = QPATHTOF(data\goggles_sand.p3d);
+        hiddenSelectionsTextures[] = {QPATHTOF(data\goggles_sand_ca.paa)};
     };
 };
