755b8aa65c07c23ed84a99582683ef1170ee9ae3
jonpas <jonpas33@gmail.com>
2015-12-13 01:16:52 +0100

    Add debug macros and more basic macros, fix pbo name prefix

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -7,5 +7,25 @@
 #define VERSION_CONFIG version = VERSION; versionStr = QUOTE(VERSION); versionAr[] = {VERSION_AR}
 #define ADDON DOUBLES(PREFIX,COMPONENT)
 
-// Internal Functions
+// Internal
 #define DOUBLES(var1,var2) ##var1##_##var2
+#define TRIPLES(var1,var2,var3) ##var1##_##var2##_##var3
+#define QUOTE(var1) #var1
+
+// Global Variable
+#define GVAR(var1) DOUBLES(ADDON,var1)
+#define EGVAR(var1,var2) TRIPLES(PREFIX,var1,var2)
+#define QGVAR(var1) QUOTE(GVAR(var1))
+#define QEGVAR(var1,var2) QUOTE(EGVAR(var1,var2))
+
+// String
+#define LSTRING(var1) QUOTE(TRIPLES(STR,ADDON,var1))
+#define ELSTRING(var1,var2) QUOTE(TRIPLES(STR,DOUBLES(PREFIX,var1),var2))
+#define CSTRING(var1) QUOTE(TRIPLES($STR,ADDON,var1))
+#define ECSTRING(var1,var2) QUOTE(TRIPLES($STR,DOUBLES(PREFIX,var1),var2))
+
+// Path
+#define PATHTOF(var1) \MAINPREFIX\PREFIX\SUBPREFIX\COMPONENT\var1
+
+// Debug Functions
+#include "script_debug.hpp"
