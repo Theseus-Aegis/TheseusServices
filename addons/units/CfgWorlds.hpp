ce5050399952f4822279b53409205ac4c41e7c2a
jonpas <jonpas33@gmail.com>
2016-02-08 03:27:07 +0100

    Add more generic names

--- addons/units/CfgWorlds.hpp
+++ addons/units/CfgWorlds.hpp
@@ -4,10 +4,16 @@ class CfgWorlds {
             class FirstNames {
                 john = "John";
                 cole = "Cole";
+                dimas = "Dimas";
+                thomas = "Thomas";
+                brent = "Brent";
             };
             class LastNames {
                 james = "James";
                 taylor = "Taylor";
+                lopez = "López";
+                mueller = "Müller";
+                knyte = "Knyte";
             };
         };
     };
