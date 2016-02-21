7b00ec4dda7612cca2d5d1bc032ce2ec1c224a46
jonpas <jonpas33@gmail.com>
2016-02-21 18:25:34 +0100

    Add Theseus Inc. flag - work on #19

--- addons/main/CfgFactionClasses.hpp
+++ addons/main/CfgFactionClasses.hpp
@@ -1,14 +1,14 @@
 class CfgFactionClasses {
     class CLASS(IND) {
         displayName = CSTRING(Company);
-        //flag = QPATHTOF(UI\flag_tac_co.paa);//@todo - Theseus flag
+        flag = QPATHTOF(UI\flag_tac_co.paa);
         icon = QPATHTOF(UI\logo_tac_small_ca.paa);
         side = 2;
         priority = 2;
     };
     class CLASS(BLU) {
         displayName = CSTRING(Company);
-        //flag = QPATHTOF(UI\flag_tac_co.paa);//@todo - Theseus flag
+        flag = QPATHTOF(UI\flag_tac_co.paa);
         icon = QPATHTOF(UI\logo_tac_small_ca.paa);
         side = 1;
         priority = 3;
