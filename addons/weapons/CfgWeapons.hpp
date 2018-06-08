3a65ce81d0c4782d02089cfc2deb2b0ca06e888b
JoramD <j.davids@hotmail.nl>
2018-06-08 12:29:38 +0200

    Walther P99 (#84)
    * Added Walther P99
    
    * Fixed authors.txt
    
    * Fixed minor issues
    
    * Added tan version, improved preview images

--- addons/weapons/CfgWeapons.hpp
+++ addons/weapons/CfgWeapons.hpp
@@ -29,6 +29,25 @@ class CfgWeapons {
         baseWeapon = QCLASS(MX_GL_BlackCamo);
     };
 
+    class hgun_P07_F;
+    class CLASS(Walther_P99): hgun_P07_F {
+        dlc = QUOTE(PREFIX);
+        scope = 2;
+        author = "JoramD";
+        displayName = CSTRING(Walther_P99);
+        picture = QPATHTOF(UI\walther_p99_ca.paa);
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\walther_p99_co.paa)};
+        baseWeapon = QCLASS(Walther_P99);
+    };
+
+    class CLASS(Walther_P99_Tan): CLASS(Walther_P99) {
+        displayName = CSTRING(Walther_P99_Tan);
+        picture = QPATHTOF(UI\walther_p99_tan_ca.paa);
+        hiddenSelections[] = {"camo"};
+        hiddenSelectionsTextures[] = {QPATHTOF(data\walther_p99_tan_co.paa)};
+        baseWeapon = QCLASS(Walther_P99_Tan);
+    };
 
     // Equipped
     class CLASS(MX_BlackCamo_MRCO): CLASS(MX_BlackCamo) {
