f6ef53920575ed045b7220680286f28664b43dbb
TyroneMF <blackbypopular-demand@hotmail.co.uk>
2019-06-10 19:04:24 +0100

    Correct Formatting
    Removed spaces after commas
    Removed spaces before :

--- addons/units/CfgVehicles_GroundItems.hpp
+++ addons/units/CfgVehicles_GroundItems.hpp
@@ -125,49 +125,49 @@ class CLASS(Item_Uniform_Combat_LS_TS_TP_TB): Item_Base_F {
         MACRO_ADDITEM(CLASS(Uniform_Combat_LS_TS_TP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_LS_CBPS_BP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_LS_CBPS_BP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_LS_CBPS_BP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CBPS_BP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CBPS_BP_BB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_LS_CDES_TP_TB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_LS_CDES_TP_TB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_LS_CDES_TP_TB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CDES_TP_TB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CDES_TP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_LS_CDFS_GP_TB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_LS_CDFS_GP_TB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_LS_CDFS_GP_TB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CDFS_GP_TB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CDFS_GP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_LS_CLES_TP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_LS_CLES_TP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_LS_CLES_TP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CLES_TP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CLES_TP_BB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_LS_CLFS_GP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_LS_CLFS_GP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_LS_CLFS_GP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CLFS_GP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_LS_CLFS_GP_BB),1);
     };
 };
 class CLASS(Item_Uniform_Combat_LS_CDBS_GP_TB): Item_Base_F {
@@ -332,49 +332,49 @@ class CLASS(Item_Uniform_Combat_RS_TS_TP_TB): Item_Base_F {
         MACRO_ADDITEM(CLASS(Uniform_Combat_RS_TS_TP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_RS_CBPS_BP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_RS_CBPS_BP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_RS_CBPS_BP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CBPS_BP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CBPS_BP_BB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_RS_CDES_TP_TB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_RS_CDES_TP_TB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_RS_CDES_TP_TB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CDES_TP_TB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CDES_TP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_RS_CDFS_GP_TB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_RS_CDFS_GP_TB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_RS_CDFS_GP_TB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CDFS_GP_TB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CDFS_GP_TB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_RS_CLES_TP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_RS_CLES_TP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_RS_CLES_TP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CLES_TP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CLES_TP_BB),1);
     };
 };
-class CLASS(Item_Uniform_Combat_RS_CLFS_GP_BB) : Item_Base_F {
+class CLASS(Item_Uniform_Combat_RS_CLFS_GP_BB): Item_Base_F {
     MACRO_ITEM_COMMON
     author = "GilleeDoo, Tyrone";
     displayName = CSTRING(Uniform_Combat_RS_CLFS_GP_BB);
 
     class TransportItems {
-        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CLFS_GP_BB), 1);
+        MACRO_ADDITEM(CLASS(Uniform_Combat_RS_CLFS_GP_BB),1);
     };
 };
 class CLASS(Item_Uniform_Combat_RS_CDBS_GP_TB): Item_Base_F {
