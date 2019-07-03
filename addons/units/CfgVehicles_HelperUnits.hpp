9de727c5074b49da097dc7c88afa07b18a9f2967
TyroneMF <TyroneMF@hotmail.com>
2019-07-03 14:04:31 +0100

    Fixed Rolled Plaid uniform base models (#108)
    Plaid should now show correctly.

--- addons/units/CfgVehicles_HelperUnits.hpp
+++ addons/units/CfgVehicles_HelperUnits.hpp
@@ -310,31 +310,31 @@ class CLASS(Unit_Combat_RS_C_Base): CLASS(Unit_Combat_RS_Base) {
     hiddenSelectionsMaterials[] = {QPATHTOF(data\uniform_combat_plaid.rvmat)};
 };
 
-class CLASS(Unit_Combat_RS_CBPS_BP_BB): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_RS_CBPS_BP_BB): CLASS(Unit_Combat_RS_Base) {
     scope = 1;
     author = "GilleeDoo, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_RS_CBPS_BP_BB);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_cbps_bp_bb_co.paa)};
 };
-class CLASS(Unit_Combat_RS_CDES_TP_TB): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_RS_CDES_TP_TB): CLASS(Unit_Combat_RS_Base) {
     scope = 1;
     author = "GilleeDoo, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_RS_CDES_TP_TB);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_cdes_tp_tb_co.paa)};
 };
-class CLASS(Unit_Combat_RS_CDFS_GP_TB): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_RS_CDFS_GP_TB): CLASS(Unit_Combat_RS_Base) {
     scope = 1;
     author = "GilleeDoo, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_RS_CDFS_GP_TB);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_cdfs_gp_tb_co.paa)};
 };
-class CLASS(Unit_Combat_RS_CLES_TP_BB): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_RS_CLES_TP_BB): CLASS(Unit_Combat_RS_Base) {
     scope = 1;
     author = "GilleeDoo, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_RS_CLES_TP_BB);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_cles_tp_bb_co.paa)};
 };
-class CLASS(Unit_Combat_RS_CLFS_GP_BB): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_RS_CLFS_GP_BB): CLASS(Unit_Combat_RS_Base) {
     scope = 1;
     author = "GilleeDoo, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_RS_CLFS_GP_BB);
