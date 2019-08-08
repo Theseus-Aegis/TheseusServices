ef70803dda896d0b9ab2b49c03277e184c3db1e5
jonpas <jonpas33@gmail.com>
2019-08-08 02:28:08 +0200

    Prepare release 1.11.0

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -1,4 +1,4 @@
 #define MAJOR 1
-#define MINOR 10
+#define MINOR 11
 #define PATCHLVL 0
-#define BUILD 15
+#define BUILD 16
--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Theseus Services 1.10.0";
+name = "Theseus Services 1.11.0";
 picture = "logo_tacs_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/Theseus-Aegis/TheseusServices";
