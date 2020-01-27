a71e5824664bd72477eb498b7cdb8f9b5e67affb
TyroneMF <TyroneMF@hotmail.com>
2020-01-27 04:04:19 +0000

    GT Arcadian
    - Reworked sounds
    - Removed sidepanels
    - Reworked speeds
    - Simulated runflat tires

---
+++ optionals/arcadian/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT gt armory arcadian
+#define COMPONENT_BEAUTIFIED GT Armory Arcadian
+
+#include "\x\tacs\addons\main\script_mod.hpp"
+
+#include "\x\tacs\addons\main\script_macros.hpp"
