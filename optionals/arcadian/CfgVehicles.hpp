6be03650689d4a4a5440a5ff09bd1a97f8f21e9c
jonpas <jonpas33@gmail.com>
2020-02-01 04:05:00 +0100

    Improve Arcadian encoding (fix inheritance and cleanup)

--- optionals/arcadian/CfgVehicles.hpp
+++ optionals/arcadian/CfgVehicles.hpp
@@ -1,13 +1,16 @@
 class CfgVehicles {
     class Car_F;
     class CUP_SUV_Base: Car_F {
-        class HitPoints {
+        class HitPoints;
+    };
+
+    class CUP_SUV_Unarmed_Base: CUP_SUV_Base {
+        class HitPoints: HitPoints {
             class HitLFWheel;
             class HitLF2Wheel;
             class HitRFWheel;
             class HitRF2Wheel;
             class HitEngine;
-            class HitFuel;
             class HitBody;
             class HitGlass1;
             class HitGlass2;
@@ -15,106 +18,75 @@ class CfgVehicles {
             class HitGlass4;
         };
 
-        class NewTurret;
         class Turrets {
-            class MainTurret: NewTurret {
-                class HitPoints;
-            };
-        };
-        class Eventhandlers;
-        class AnimationSources;
-    };
-
-    class CUP_SUV_Unarmed_Base: CUP_SUV_Base {
-        class AnimationSources: AnimationSources {
-            class rear_window_source {
-                source = "door";
-                initPhase = 0;
-                animPeriod = 1;
-            };
+            class CargoTurret_09;
         };
     };
 
+    // Base
     class CLASS(Arcadian_Base): CUP_SUV_Unarmed_Base {
         dlc = QUOTE(PREFIX);
         scope = 1;
-        author = "Tyrone, Gilleedoo";
+        author = "TyroneMF, GilleeDoo";
 
+        // Damage
         armor = 90;
-        crewCrashProtection = 0.25;
-        hiddenSelections[] = {"camo"};
-
-        // Damage & Hitpoints
         class HitPoints: HitPoints {
             class HitLFWheel: HitLFWheel {
                 armor = 200;
-                explosionShielding = 2;
                 passThrough = 0;
-                radius = 0.33000001;
+                explosionShielding = 2;
             };
             class HitLF2Wheel: HitLF2Wheel {
                 armor = 200;
-                explosionShielding = 2;
                 passThrough = 0;
-                radius = 0.33000001;
+                explosionShielding = 2;
             };
             class HitRFWheel: HitRFWheel {
                 armor = 200;
-                explosionShielding = 2;
                 passThrough = 0;
-                radius = 0.33000001;
+                explosionShielding = 2;
             };
             class HitRF2Wheel: HitRF2Wheel {
                 armor = 200;
-                explosionShielding = 2;
                 passThrough = 0;
-                radius = 0.33000001;
+                explosionShielding = 2;
             };
             class HitEngine: HitEngine {
                 armor = 0.5;
-                material = -1;
-                name = "engine";
-                visual = "-";
                 passThrough = 0.30000001;
                 explosionShielding = 0.2;
             };
             class HitBody: HitBody {
                 armor = 3;
-                material = -1;
-                name = "body";
-                visual = "camo";
                 passThrough = 1;
                 explosionShielding = 1.5;
-                radius = 0.44999999;
             };
             class HitGlass1: HitGlass1 {
                 armor = 1;
                 explosionShielding = 3;
-                radius = 0.5;
                 passThrough = 0;
             };
             class HitGlass2: HitGlass2 {
                 armor = 1;
                 explosionShielding = 3;
-                radius = 0.5;
                 passThrough = 0;
             };
             class HitGlass3: HitGlass3 {
                 armor = 1;
                 explosionShielding = 3;
-                radius = 0.5;
                 passThrough = 0;
             };
             class HitGlass4: HitGlass4 {
                 armor = 1;
                 explosionShielding = 3;
-                radius = 0.5;
                 passThrough = 0;
             };
         };
-        wheelDamageThreshold = 0.18000001;
-        wheelDamageRadiusCoef = 0.75;
-        wheelDestroyRadiusCoef = 0.47999999;
+
+        accuracy = 0.35; // Increase targeting priority for AI due to more durable vehicle (default: 0.25)
+
+        // Sound
         soundEngineOnInt[] = {"A3\Sounds_F\vehicles2\soft\Mrap_01\Mrap_01_Engine_Int_Start", 0.630957, 1};
         soundEngineOnExt[] = {"A3\Sounds_F\vehicles2\soft\Mrap_01\Mrap_01_Engine_Ext_Start", 1.995260, 1, 50};
         soundEngineOffInt[] = {"A3\Sounds_F\vehicles2\soft\Mrap_01\Mrap_01_Engine_Int_stop", 0.501187, 1};
@@ -122,14 +94,13 @@ class CfgVehicles {
 
         #include "sounds.hpp"
 
+        // Driving (source: Vanilla Hunter)
         thrustDelay = 0.1;
         brakeIdleSpeed = 1.78;
         maxSpeed = 200;
-        fuelCapacity = 45;
         idleRpm = 800;
         redRpm = 4500;
 
-        // Source: Vanilla Hunter
         class complexGearbox {
             gearboxRatios[] = {
                 "R1", -4,
@@ -162,45 +133,22 @@ class CfgVehicles {
             {"(2000/3500)", "(425/425)"},
             {"(2500/3500)", "(350/425)"},
             {"(3000/3500)", "(300/425)"},
-            {"(6000/3500)","(0/425)"}
+            {"(6000/3500)", "(0/425)"}
         };
-        changeGearMinEffectivity[] = {0.94999999, 0.15000001, 0.94999999, 0.94999999, 0.94999999, 0.94999999, 0.94999999}; // Default {1, 0.15000001, 1, 1, 1, 1, 1, 1};
+        changeGearMinEffectivity[] = {0.94999999, 0.15000001, 0.94999999, 0.94999999, 0.94999999, 0.94999999, 0.94999999}; // Default: {1, 0.15, 1, 1, 1, 1, 1, 1}
         switchTime = 0.31;
         latency = 1.5;
 
-        class CargoTurret;
+        // Turrets (remove side mounts)
         class Turrets: Turrets {
-            delete CargoTurret_03;
-            delete CargoTurret_04;
-            delete CargoTurret_05;
-            delete CargoTurret_06;
-            delete CargoTurret_07;
-            delete CargoTurret_08;
-            class CargoTurret_09: CargoTurret {
-                minElev = -10;
-                maxElev = 18;
-                maxTurn = -10;
-                minTurn = -110;
-                gunnerAction = "passenger_boat_4";
-                gunnerName = "$str_a3_rear_gunner";
-                proxyIndex = 13;
-                isPersonTurret = 1;
-                memoryPointsGetInGunner = "pos cargo rear";
-                memoryPointsGetInGunnerDir = "pos cargo dir rear";
-                memoryPointGunnerOptics = "";
-                gunnerOutOpticsModel = "";
-                gunnerOpticsModel = "";
-                enabledByAnimationSource = "rear_window_source";
-                usepip = 0;
-                startEngine = 0;
-                outGunnerMayFire = 1;
-                inGunnerMayFire = 0;
-                commanding = -2;
+            class CargoTurret_09: CargoTurret_09 {
+                gunnerName = "$STR_A3_Rear_Gunner"; // Fix rear gunner seat name
             };
         };
     };
 
-    // TODO I first, then B
+
+    // Public
     class CLASS(Arcadian_I_Black): CLASS(Arcadian_Base) {
         scope = 2;
         scopeCurator = 2;
@@ -209,13 +157,11 @@ class CfgVehicles {
         faction = QCLASS(IND);
         displayName = CSTRING(Black);
         editorPreview = QPATHTOF(ui\Arcadian.jpg);
-        crew = QCLASS(Unit_B_Contractor);
-        typicalCargo[] = {QCLASS(Unit_B_Contractor)};
+        crew = QCLASS(Unit_I_Contractor);
+        typicalCargo[] = {QCLASS(Unit_I_Contractor)};
         hiddenSelectionsTextures[] = {
             QPATHTOF(data\arcadian_body_black_co.paa)
         };
-
-        accuracy = 0.35; // Increase targeting priority for AI due to more durable vehicle (default: 0.25)
     };
     class CLASS(Arcadian_B_Black): CLASS(Arcadian_I_Black) {
         scope = 2;
@@ -231,7 +177,7 @@ class CfgVehicles {
         displayName = CSTRING(Tan);
         editorPreview = QPATHTOF(ui\Arcadian_Tan.jpg);
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\arcadian_body_earth_co.paa)
+            QPATHTOF(data\arcadian_body_tan_co.paa)
         };
     };
     class CLASS(Arcadian_B_Tan): CLASS(Arcadian_I_Tan) {
@@ -248,7 +194,7 @@ class CfgVehicles {
         displayName = CSTRING(Green);
         editorPreview = QPATHTOF(ui\Arcadian_Green.jpg);
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\arcadian_body_foliage_co.paa)
+            QPATHTOF(data\arcadian_body_green_co.paa)
         };
     };
     class CLASS(Arcadian_B_Green): CLASS(Arcadian_I_Green) {
---
+++ optionals/arcadian/README.md
@@ -0,0 +1,8 @@
+# About
+
+Adds Arcadian GT vehicle, a modified [CUP](http://cup-arma3.org/) SUV featuring armor plates, sturdier windows, better engine and removal of side mounts.
+
+### Authors
+
+- [TyroneMF](https://github.com/TyroneMF)
+- GilleeDoo
