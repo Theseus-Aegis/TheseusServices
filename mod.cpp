62eca3273ea1c7c88e39be4eb6a3805fcca42642
TyroneMF <TyroneMF@hotmail.com>
2020-11-10 14:12:17 +0000

    Prepare release 1.17.0

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Theseus Services 1.16.1";
+name = "Theseus Services 1.17.0";
 picture = "logo_tacs_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/Theseus-Aegis/TheseusServices";
