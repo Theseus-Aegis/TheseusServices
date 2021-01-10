dd0a9d385b878be70de30a0e08f547e7e9a9bd0c
TyroneMF <TyroneMF@hotmail.com>
2021-01-10 18:58:03 +0000

    Arcadian - Increase default cargo space from 4 to 6 (#149)
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- optionals/arcadian/CfgVehicles.hpp
+++ optionals/arcadian/CfgVehicles.hpp
@@ -191,6 +191,9 @@ class CfgVehicles {
         class Turrets: Turrets {
             class CargoTurret_09: CargoTurret_09 {};
         };
+
+        // Increase cargo space (default: 4)
+        ace_cargo_space = 6;
     };
 
 
