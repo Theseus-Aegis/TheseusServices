649d3e702a43965f3fd94ffddc06023e38957893
jonpas <jonpas33@gmail.com>
2021-01-16 17:14:28 +0100

    Prepare release 1.17.1

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
-name = "Theseus Services 1.17.0";
+name = "Theseus Services 1.17.1";
 picture = "logo_tacs_ca.paa";
 actionName = "GitHub";
 action = "https://github.com/Theseus-Aegis/TheseusServices";
