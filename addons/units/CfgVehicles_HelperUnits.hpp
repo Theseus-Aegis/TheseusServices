a62660eb5fa40e2ea2a43775fe8515fded339d14
TyroneMF <TyroneMF@hotmail.com>
2021-02-03 22:51:27 +0000

    initial push

--- addons/units/CfgVehicles_HelperUnits.hpp
+++ addons/units/CfgVehicles_HelperUnits.hpp
@@ -71,6 +71,34 @@ class CLASS(Unit_Combat_LS_Base): B_Soldier_base_F {
     hiddenSelectionsMaterials[] = {QPATHTOF(data\uniform_combat.rvmat)};
 };
 
+class CLASS(Unit_Combat_LS_ATACS_ATX): CLASS(Unit_Combat_LS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_LS_ATACS_ATX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_atx_co.paa)};
+};
+
+class CLASS(Unit_Combat_LS_ATACS_AUX): CLASS(Unit_Combat_LS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_LS_ATACS_AUX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_aux_co.paa)};
+};
+
+class CLASS(Unit_Combat_LS_ATACS_IX): CLASS(Unit_Combat_LS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_LS_ATACS_IX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_ix_co.paa)};
+};
+
+class CLASS(Unit_Combat_LS_ATACS_LEX): CLASS(Unit_Combat_LS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_LS_ATACS_LEX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_lex_co.paa)};
+};
+
 class CLASS(Unit_Combat_LS_BS_BP_BB): CLASS(Unit_Combat_LS_Base) {
     scope = 1;
     author = "Pomigit, Kresky";
@@ -224,6 +252,34 @@ class CLASS(Unit_Combat_RS_Base): B_Soldier_03_f {
     MACRO_BASE_CLASS_COMMON
     nakedUniform = QCLASS(Underwear_WhiteLogo);
     hiddenSelectionsMaterials[] = {QPATHTOF(data\uniform_combat.rvmat)};
+};
+
+class CLASS(Unit_Combat_RS_ATACS_ATX): CLASS(Unit_Combat_RS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_RS_ATACS_ATX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_atx_co.paa)};
+};
+
+class CLASS(Unit_Combat_RS_ATACS_AUX): CLASS(Unit_Combat_RS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_RS_ATACS_AUX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_aux_co.paa)};
+};
+
+class CLASS(Unit_Combat_RS_ATACS_IX): CLASS(Unit_Combat_RS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_RS_ATACS_IX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_ix_co.paa)};
+};
+
+class CLASS(Unit_Combat_RS_ATACS_LEX): CLASS(Unit_Combat_RS_Base) {
+    scope = 1;
+    author = "Tibernius, Tyrone";
+    uniformClass = QCLASS(Uniform_Combat_RS_ATACS_LEX);
+    hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_lex_co.paa)};
 };
 
 class CLASS(Unit_Combat_RS_BS_BP_BB): CLASS(Unit_Combat_RS_Base) {
--- addons/units/CfgWeapons.hpp
+++ addons/units/CfgWeapons.hpp
@@ -58,8 +58,98 @@ class CfgWeapons {
         };
     };
 
-    // Naming pattern: TypeSleevesOrPants_ColorShirt_ColorPants_ColorBoots
+    // Naming pattern: Sleeve length_Camo_CamoName
+    // Combat ATACS - Long Sleeves
+    class CLASS(Uniform_Combat_LS_ATACS_ATX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_LS_ATACS_ATX);
+        picture = QPATHTOF(UI\uniform_combat_ls_atacs_atx.paa);
 
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_LS_ATACS_ATX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_LS_ATACS_AUX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_LS_ATACS_AUX);
+        picture = QPATHTOF(UI\uniform_combat_ls_atacs_aux.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_LS_ATACS_AUX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_LS_ATACS_IX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_LS_ATACS_IX);
+        picture = QPATHTOF(UI\uniform_combat_ls_atacs_ix.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_LS_ATACS_IX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_LS_ATACS_LEX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_LS_ATACS_LEX);
+        picture = QPATHTOF(UI\uniform_combat_ls_atacs_lex.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_LS_ATACS_LEX);
+        };
+    };
+
+    // Combat ATACS - Short Sleeves
+    class CLASS(Uniform_Combat_RS_ATACS_ATX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_RS_ATACS_ATX);
+        picture = QPATHTOF(UI\uniform_combat_rs_atacs_atx.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_RS_ATACS_ATX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_RS_ATACS_AUX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_RS_ATACS_AUX);
+        picture = QPATHTOF(UI\uniform_combat_rs_atacs_aux.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_RS_ATACS_AUX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_RS_ATACS_IX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_RS_ATACS_IX);
+        picture = QPATHTOF(UI\uniform_combat_rs_atacs_ix.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_RS_ATACS_IX);
+        };
+    };
+
+    class CLASS(Uniform_Combat_RS_ATACS_LEX): CLASS(Uniform_Base) {
+        scope = 2;
+        author = "Tibernius, Tyrone";
+        displayName = CSTRING(Uniform_Combat_RS_ATACS_LEX);
+        picture = QPATHTOF(UI\uniform_combat_rs_atacs_lex.paa);
+
+        class ItemInfo: ItemInfo {
+            uniformClass = QCLASS(Unit_Combat_RS_ATACS_LEX);
+        };
+    };
+
+    // Naming pattern: TypeSleevesOrPants_ColorShirt_ColorPants_ColorBoots
     // Combat - Long SLeeves
     class CLASS(Uniform_Combat_LS_BS_BP_BB): CLASS(Uniform_Base) {
         scope = 2;
