7ab89c47845b23d6eb3ea51a7617f75f2462dd1f
TyroneMF <TyroneMF@hotmail.com>
2021-02-04 20:00:19 +0000

    Fix filepath & macro

--- addons/units/items/base_item.hpp
+++ addons/units/items/base_item.hpp
@@ -4,7 +4,7 @@
     scopeCurator = 2; \
     vehicleClass = "ItemsUniforms"; \
     model = "\A3\Weapons_F\DummySuitpack.p3d"; \
-    displayName = CSTRING(item) \
+    displayName = CSTRING(item); \
     class TransportItems { \
         MACRO_ADDITEM(CLASS(item),1); \
     };
