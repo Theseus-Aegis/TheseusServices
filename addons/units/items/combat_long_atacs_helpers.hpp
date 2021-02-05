8af84e3f07944e53c562f33309634b2a8fa4ee06
TyroneMF <TyroneMF@hotmail.com>
2021-02-05 09:51:53 +0000

    initial push

--- addons/units/items/combat_long_atacs_helpers.hpp
+++ addons/units/items/combat_long_atacs_helpers.hpp
@@ -1,24 +1,24 @@
 // Naming pattern: SleeveType_Camo_Pattern
 // Combat ATACS - Long Sleeves
-class CLASS(Unit_Combat_LS_ATACS_ATX): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_LS_ATACS_ATX): CLASS(Unit_Combat_LS_Atacs_Base) {
     scope = 1;
     author = "Tibernius, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_LS_ATACS_ATX);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_atx_co.paa)};
 };
-class CLASS(Unit_Combat_LS_ATACS_AUX): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_LS_ATACS_AUX): CLASS(Unit_Combat_LS_Atacs_Base) {
     scope = 1;
     author = "Tibernius, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_LS_ATACS_AUX);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_aux_co.paa)};
 };
-class CLASS(Unit_Combat_LS_ATACS_IX): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_LS_ATACS_IX): CLASS(Unit_Combat_LS_Atacs_Base) {
     scope = 1;
     author = "Tibernius, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_LS_ATACS_IX);
     hiddenSelectionsTextures[] = {QPATHTOF(data\uniform_combat_atacs_ix_co.paa)};
 };
-class CLASS(Unit_Combat_LS_ATACS_LEX): CLASS(Unit_Combat_LS_Base) {
+class CLASS(Unit_Combat_LS_ATACS_LEX): CLASS(Unit_Combat_LS_Atacs_Base) {
     scope = 1;
     author = "Tibernius, Tyrone";
     uniformClass = QCLASS(Uniform_Combat_LS_ATACS_LEX);
