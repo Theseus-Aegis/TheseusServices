dadbc0502167f1dabad6f742986d8c4290de4cb4
Lairdd1989 <54322466+Lairdd1989@users.noreply.github.com>
2021-02-24 11:04:14 +0000

    Add more Insignias (#155)
    * Add Insignia
    
    Adds following insignia's
    
    -Brand Of Sacrifice
    -Bridge Four
    -I Am Unstoppable
    -Judge
    -Natural Twenty
    -Never Forget
    -Raptor Squad
    
    * Update from code review
    
    fix spacing and indentation
    
    * Casing
    
    * More casing
    
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/insignias/config.cpp
+++ addons/insignias/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches {
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {"tacs_main"};
         author = ECSTRING(main,Author);
-        authors[] = {"Jonpas", "BadHabitz", "System98", "GilleeDoo"};
+        authors[] = {"Jonpas", "BadHabitz", "System98", "GilleeDoo", "Lairdd1989"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
     };
