b2413a28c346829f52fd9a7f8ede22e2bf12f881
TyroneMF <TyroneMF@hotmail.com>
2021-07-25 19:49:43 +0100

    Fix Radio Z-fighting (Probably?) Set "Open Rear Window" to lower priority.

--- addons/arcadian/base/config_useractions.hpp
+++ addons/arcadian/base/config_useractions.hpp
@@ -5,7 +5,7 @@ class UserActions {
         position = "";
         radius = 3.7;
         onlyForPlayer = 0;
-        priority = 10;
+        priority = 0;
         condition = "this doorPhase 'reardoor_2_source' < 0.5 && {(this getCargoIndex (missionNamespace getVariable ['bis_fnc_moduleRemoteControl_unit', player])) isEqualTo 0}";
         statement = "this animateDoor ['reardoor_2_source', 1];";
     };
