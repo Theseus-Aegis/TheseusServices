e34c32b9d6e86101bd1e6f7422a5f10294cdd975
TyroneMF <TyroneMF@hotmail.com>
2021-09-01 00:04:44 +0100

    Attempt 1

--- addons/arcadian/base/baseclass.hpp
+++ addons/arcadian/base/baseclass.hpp
@@ -1,13 +1,11 @@
 class LandVehicle;
 class Car: LandVehicle {
-    class ACE_Actions {
-        class ACE_MainActions;
-    };
+    class ACE_Actions;
 };
 
 class Car_F: Car {
     class ACE_Actions: ACE_Actions {
-        class ACE_MainActions: ACE_MainActions {
+        class ACE_MainActions;
     };
 
     class AnimationSources;
