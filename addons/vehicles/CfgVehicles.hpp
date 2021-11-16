71e37b2ad567f305945e796065e90c12622c5fe8
JoramD <j.davids@hotmail.nl>
2021-11-16 15:21:55 +0100

    Add Theseus RHIB (#180)
    Co-authored-by: jonpas <jonpas33@gmail.com>

--- addons/vehicles/CfgVehicles.hpp
+++ addons/vehicles/CfgVehicles.hpp
@@ -2,5 +2,6 @@ class CfgVehicles {
     #include "vehicles\mh6.hpp"
     #include "vehicles\offroad.hpp"
     #include "vehicles\polaris.hpp"
+    #include "vehicles\rhib.hpp"
     #include "vehicles\van.hpp"
 };
