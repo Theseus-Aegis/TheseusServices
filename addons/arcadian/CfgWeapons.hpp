a0e6b5f75c6992ecd1126f04321c70c28a7009ac
Mike-MF <TyroneMF@hotmail.com>
2022-11-05 21:36:53 +0000

    Switch Arcadian magazine to yellow tracer

--- addons/arcadian/CfgWeapons.hpp
+++ addons/arcadian/CfgWeapons.hpp
@@ -2,5 +2,6 @@ class CfgWeapons {
     class LMG_Minigun_Transport;
     class CLASS(LMG_Minigun_SUV): LMG_Minigun_Transport {
         displayName = "M134 Minigun 6.5mm";
+        magazines[] = {"2000Rnd_65x39_Belt_Tracer_Yellow", "1000Rnd_65x39_Belt_Tracer_Yellow"};
     };
 };
