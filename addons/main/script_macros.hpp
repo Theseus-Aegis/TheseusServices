2616ee7cbc9fc918cf55f3d7bd3c65bf0cb7e7c5
jonpas <jonpas33@gmail.com>
2023-04-18 18:27:26 +0200

    Adapt to HEMTT v1 (#206)

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,6 +1,6 @@
 // Internal
-#define DOUBLES(var1,var2) ##var1##_##var2
-#define TRIPLES(var1,var2,var3) ##var1##_##var2##_##var3
+#define DOUBLES(var1,var2) var1##_##var2
+#define TRIPLES(var1,var2,var3) var1##_##var2##_##var3
 #define ADDON DOUBLES(PREFIX,COMPONENT)
 
 // Main
