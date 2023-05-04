5a256be9cbb321d12fe5f2b58e8fb8f39575e37a
Joram Davids <j.davids@hotmail.nl>
2023-05-04 22:19:16 +0200

    Initial push

---
+++ optionals/variants_milgp/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT variants_milgp
+#define COMPONENT_BEAUTIFIED MilGP Variants
+#include "\x\tacs\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_VARIANTS_MILGP
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_VARIANTS_MILGP
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_VARIANTS_MILGP
+#endif
+
+#include "\x\tacs\addons\main\script_macros.hpp"
