fe45b476bcb27779e22a4c8268caa7f03620190d
jonpas <jonpas33@gmail.com>
2023-06-15 14:20:43 +0200

    Optional Aegis Variants - Add Theseus C-130J retexture (#211)

---
+++ optionals/variants_aegis/CfgVehicles.hpp
@@ -0,0 +1,45 @@
+class CfgVehicles {
+    class Plane_Transport_01_base_F;
+
+    class CLASS(C130J_Aegis_I_Theseus): Plane_Transport_01_base_F {
+        MACRO_AIRVEHICLE_CARGO
+        dlc = QUOTE(PREFIX);
+        scope = 2;
+        forceInGarage = 1;
+        side = 2;
+        faction = QCLASS(IND);
+        author = CSTRING(C130J_Author);
+        displayName = CSTRING(C130J_Theseus_Name);
+        editorPreview = QPATHTOF(ui\C130J_Theseus.jpg);
+        crew = QCLASS(Unit_I_PilotHeli);
+        typicalCargo[] = {QCLASS(Unit_I_Engineer)};
+        hiddenSelections[] = {"camo1", "camo2"};
+        hiddenSelectionsTextures[] = {
+            QPATHTOF(data\theseus_c130j_body_co.paa),
+            QPATHTOF(data\theseus_c130j_wings_co.paa)
+        };
+        class TextureSources {
+            class ADDON {
+                displayname = CSTRING(C130J_Theseus_Texture_Name);
+                author = CSTRING(C130J_Author);
+                textures[] = {
+                    QPATHTOF(data\theseus_c130j_body_co.paa),
+                    QPATHTOF(data\theseus_c130j_wings_co.paa)
+                };
+                factions[] = {
+                    QCLASS(IND),
+                    QCLASS(BLU)
+                };
+            };
+        };
+    };
+
+    class CLASS(C130J_Aegis_B_Theseus): CLASS(C130J_Aegis_I_Theseus) {
+        scope = 2;
+        forceInGarage = 0;
+        side = 1;
+        faction = QCLASS(BLU);
+        crew = QCLASS(Unit_B_PilotHeli);
+        typicalCargo[] = {QCLASS(Unit_B_Engineer)};
+    };
+};
---
+++ optionals/variants_aegis/script_component.hpp
@@ -0,0 +1,6 @@
+#define COMPONENT variants_aegis
+#define COMPONENT_BEAUTIFIED Aegis Variants
+
+#include "\x\tacs\addons\main\script_mod.hpp"
+
+#include "\x\tacs\addons\main\script_macros.hpp"
