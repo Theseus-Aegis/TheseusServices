485d854f237a0ee411ea9017368c0c715cad64dc
Mike-MF <TyroneMF@hotmail.com>
2024-03-28 15:33:07 +0000

    Fixed classnames & added displayNames

--- addons/variants_cup/CfgVehicles.hpp
+++ addons/variants_cup/CfgVehicles.hpp
@@ -1,202 +1,202 @@
 class CfgVehicles {
     class CUP_I_PMC_Soldier_42;
-    class CLASS(unit_gd_black_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_black): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Black);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_black_kneeless);
+        uniformClass = QCLASS(uniform_long_black);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_black1_kneeless_co.paa),
+            QPATHTOF(data\uniform_black1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_earth_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_earth): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Earth);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_earth_kneeless);
+        uniformClass = QCLASS(uniform_long_earth);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_earth1_kneeless_co.paa),
+            QPATHTOF(data\uniform_earth1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_pattern_foliage_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_pattern_foliage): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Pattern_Foliage);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_pattern_foliage_kneeless);
+        uniformClass = QCLASS(uniform_long_pattern_foliage);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_pattern_foliage1_kneeless_co.paa),
+            QPATHTOF(data\uniform_pattern_foliage1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_black1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_black1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Black);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_black1_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_black1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_black1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_black1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_black2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_black2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Black2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_black2_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_black2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_black2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_black2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_buffalo1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_buffalo1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Buffalo);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_buffalo1_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_buffalo1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_buffalo1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_buffalo1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_buffalo2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_buffalo2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Buffalo2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_buffalo2_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_buffalo2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_buffalo2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_buffalo2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_earth1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_earth1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Earth);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_earth1_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_earth1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_earth1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_earth1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_earth2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_earth2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Earth2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_earth2_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_earth2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_earth2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_earth2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_earth3_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_earth3): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Earth3);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_earth3_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_earth3);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_earth3_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_earth3_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_foliage1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_foliage1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_foliage1_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_foliage1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_foliage1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_foliage1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_foliage2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_foliage2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_foliage2_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_foliage2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_foliage2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_foliage2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_foliage3_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_foliage3): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage3);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_foliage3_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_foliage3);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_foliage3_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_foliage3_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_mm1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_mm1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_MM1);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_mm1_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_mm1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_mm1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_mm1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_mm2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_mm2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_MM2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_mm2_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_mm2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_mm2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_mm2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_mm3_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_mm3): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_MM3);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_mm3_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_mm3);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_mm3_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_mm3_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_mm4_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_mm4): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_MM4);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_mm4_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_mm4);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_mm4_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_mm4_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_plaid_mm5_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_plaid_mm5): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_Plaid_MM5);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_plaid_mm5_kneeless);
+        uniformClass = QCLASS(uniform_long_plaid_mm5);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_mm5_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_mm5_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_white1_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_white1): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_White);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_white1_kneeless);
+        uniformClass = QCLASS(uniform_long_white1);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_white1_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_white1_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
-    class CLASS(unit_gd_white2_kneeless): CUP_I_PMC_Soldier_42 {
-        displayName = "PLACEHOLDER UNIFORM";
+    class CLASS(unit_long_white2): CUP_I_PMC_Soldier_42 {
+        displayName = CSTRING(Uniform_Long_White2);
         author = "GilleeDoo";
-        uniformClass = QCLASS(uniform_gd_white2_kneeless);
+        uniformClass = QCLASS(uniform_long_white2);
         hiddenSelections[] = {"camo", "camo3"};
         hiddenSelectionsTextures[] = {
-            QPATHTOF(data\uniform_gd_plaid_white2_kneeless_co.paa),
+            QPATHTOF(data\uniform_plaid_white2_co.paa),
             "\CUP\Creatures\People\Military\CUP_Creatures_People_Military_PMC\data\oakley_grey_co.paa"
         };
     };
--- addons/variants_cup/CfgWeapons.hpp
+++ addons/variants_cup/CfgWeapons.hpp
@@ -4,164 +4,164 @@ class CfgWeapons {
     class CLASS(Uniform_Base): Uniform_Base {
         class ItemInfo;
     };
-    class CLASS(uniform_gd_black_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_black): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Black);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_black_kneeless);
+            uniformClass = QCLASS(unit_long_black);
         };
     };
-    class CLASS(uniform_gd_earth_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_earth): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Earth);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_earth_kneeless);
+            uniformClass = QCLASS(unit_long_earth);
         };
     };
-    class CLASS(uniform_gd_pattern_foliage_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_pattern_foliage): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Pattern_Foliage);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_pattern_foliage_kneeless);
+            uniformClass = QCLASS(unit_long_pattern_foliage);
         };
     };
-    class CLASS(uniform_gd_plaid_black1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_black1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Black);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_black1_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_black1);
         };
     };
-    class CLASS(uniform_gd_plaid_black2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_black2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Black2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_black2_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_black2);
         };
     };
-    class CLASS(uniform_gd_plaid_buffalo1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_buffalo1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Buffalo);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_buffalo1_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_buffalo1);
         };
     };
-    class CLASS(uniform_gd_plaid_buffalo2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_buffalo2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Buffalo2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_buffalo2_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_buffalo2);
         };
     };
-    class CLASS(uniform_gd_plaid_earth1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_earth1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Earth);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_earth1_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_earth1);
         };
     };
-    class CLASS(uniform_gd_plaid_earth2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_earth2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Earth2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_earth2_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_earth2);
         };
     };
-    class CLASS(uniform_gd_plaid_earth3_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_earth3): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Earth3);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_earth3_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_earth3);
         };
     };
-    class CLASS(uniform_gd_plaid_foliage1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_foliage1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_foliage1_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_foliage1);
         };
     };
-    class CLASS(uniform_gd_plaid_foliage2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_foliage2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_foliage2_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_foliage2);
         };
     };
-    class CLASS(uniform_gd_plaid_foliage3_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_foliage3): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_Foliage3);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_foliage3_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_foliage3);
         };
     };
-    class CLASS(uniform_gd_plaid_mm1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_mm1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_MM1);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_mm1_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_mm1);
         };
     };
-    class CLASS(uniform_gd_plaid_mm2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_mm2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_MM2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_mm2_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_mm2);
         };
     };
-    class CLASS(uniform_gd_plaid_mm3_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_mm3): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_MM3);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_mm3_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_mm3);
         };
     };
-    class CLASS(uniform_gd_plaid_mm4_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_mm4): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_MM4);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_mm4_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_mm4);
         };
     };
-    class CLASS(uniform_gd_plaid_mm5_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_plaid_mm5): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_Plaid_MM5);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_plaid_mm5_kneeless);
+            uniformClass = QCLASS(unit_long_plaid_mm5);
         };
     };
-    class CLASS(uniform_gd_white1_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_white1): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_White);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_white1_kneeless);
+            uniformClass = QCLASS(unit_long_white1);
         };
     };
-    class CLASS(uniform_gd_white2_kneeless): CLASS(Uniform_Base) {
+    class CLASS(uniform_long_white2): CLASS(Uniform_Base) {
         scope = 2;
-        displayName = "PLACEHOLDER";
+        displayName = CSTRING(Uniform_Long_White2);
         //picture = "";
         class ItemInfo: ItemInfo {
-            uniformClass = QCLASS(unit_gd_white2_kneeless);
+            uniformClass = QCLASS(unit_long_white2);
         };
     };
 };
