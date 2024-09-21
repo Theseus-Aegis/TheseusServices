b35f9db79cacc2c05e311218eda050d25b08964a
Mike-MF <TyroneMF@hotmail.com>
2024-09-21 22:48:39 +0100

    UI image

--- addons/units/uniforms/combat_long_auscam.hpp
+++ addons/units/uniforms/combat_long_auscam.hpp
@@ -13,7 +13,7 @@ class CLASS(Uniform_Combat_LS_AUSCAM_Khk): CLASS(Uniform_Base) {
     scope = 2;
     author = "GilleeDoo, Mike";
     displayName = CSTRING(Uniform_Combat_LS_AUSCAM_Khk);
-    //picture = QPATHTOF(UI\uniform_combat_ls_auscam_khk_ca.paa);
+    picture = QPATHTOF(UI\uniform_combat_ls_auscam_khk_ca.paa);
 
     class ItemInfo: ItemInfo {
         uniformClass = QCLASS(Unit_Combat_LS_AUSCAM_Khk);
