e0ccd4a6ccffff9e9839d9000ce54e1cd819b116
Mike-MF <TyroneMF@hotmail.com>
2024-09-22 16:07:50 +0100

    Units - Add AUSCAM Khaki (#235)

--- addons/units/items/combat_rolled_auscam_item.hpp
+++ addons/units/items/combat_rolled_auscam_item.hpp
@@ -2,3 +2,7 @@ class CLASS(Item_Uniform_Combat_RS_AUSCAM): Item_Base_F {
     MACRO_ITEM_COMMON(Uniform_Combat_RS_AUSCAM);
     author = "GilleeDoo, Mike";
 };
+class CLASS(Item_Uniform_Combat_RS_AUSCAM_Khk): Item_Base_F {
+    MACRO_ITEM_COMMON(Uniform_Combat_RS_AUSCAM_Khk);
+    author = "GilleeDoo, Mike";
+};
--- addons/units/uniforms/combat_rolled_auscam.hpp
+++ addons/units/uniforms/combat_rolled_auscam.hpp
@@ -9,3 +9,13 @@ class CLASS(Uniform_Combat_RS_AUSCAM): CLASS(Uniform_Base) {
         uniformClass = QCLASS(Unit_Combat_RS_AUSCAM);
     };
 };
+class CLASS(Uniform_Combat_RS_AUSCAM_Khk): CLASS(Uniform_Base) {
+    scope = 2;
+    author = "GilleeDoo, Mike";
+    displayName = CSTRING(Uniform_Combat_RS_AUSCAM_Khk);
+    //picture = QPATHTOF(UI\uniform_combat_ls_auscam_khk_ca.paa);
+
+    class ItemInfo: ItemInfo {
+        uniformClass = QCLASS(Unit_Combat_RS_AUSCAM_Khk);
+    };
+};
